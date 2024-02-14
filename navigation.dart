60c678975e6f05d5a52a96c25c94d4b54bddadf8
Oguzhan Cetin <oguzhancetib@gmail.com>
2024-02-14 15:51:53 +0300

    Navigation eklendi & dosyalarda düzenlemeler yapıldı

---
+++ navigation.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(Navigation());
+}
+
+class Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: _pages[_selectedIndex], 
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_outlined),
+            label: 'Raporlar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Ana Sayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex, 
+        selectedItemColor: Color(0xFF325A3E), 
+        unselectedItemColor: Colors.black, 
+        onTap: _onItemTapped, 
+      ),
+    );
+  }
+
+  // Sayfaların listesi
+  final List<Widget> _pages = [
+    Container(color: Colors.white, child: Center(child: Text('Raporlar'))),
+    Container(color: Colors.white, child: Center(child: Text('Ana Sayfa'))),
+    Container(color: Colors.white, child: Center(child: Text('Profil'))),
+  ];
+
+  
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+ 
+  }
+}
+
